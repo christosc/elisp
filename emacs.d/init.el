2c792e569b091cfae2ac3a81ed18ccbbf3c7c9df
Christos Chryssochoidis <christos.chryssochoidis@nokia.com>
2020-12-29 12:39:29 +0200

    Search tags file recursively upwards.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -418,3 +418,8 @@ There are two things you can do about this warning:
 (setq completion-ignore-case  t)
 (setq read-file-name-completion-ignore-case t)
 (setq read-buffer-completion-ignore-case t)
+
+(let ((my-tags-file (locate-dominating-file default-directory "TAGS")))
+  (when my-tags-file
+    (message "Loading tags file: %s" my-tags-file)
+        (visit-tags-table my-tags-file)))
