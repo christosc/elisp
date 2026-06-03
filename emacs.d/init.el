633d9736a0559823e97fa141df1ac4081fedf181
Chryssochoidis Christos <c.chryssochoidis@gmail.com>
2026-06-03 18:01:45 +0300

    Speed up start up

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -70,6 +70,7 @@
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
 
